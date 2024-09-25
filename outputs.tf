4364f1a00a7a9939e86bad63f20f222cf522f710
Aijaz Khan <aijazkhan405@gmail.com>
2024-09-26 11:29:06 +1200

    adding db sg id to rds

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "dns-name" {
+output "dns_name" {
   value = aws_lb.test.dns_name
 }
--- rds.tf
+++ rds.tf
@@ -8,7 +8,7 @@ resource "aws_db_instance" "twotier_rds" {
   password = var.db_password
   parameter_group_name = "default.mysql5.7"
   db_subnet_group_name = aws_db_subnet_group.twotier_dbsubnetgroup.id
-  vpc_security_group_ids = []
+  vpc_security_group_ids = [aws_security_group.twotier_db_sg.id]
   skip_final_snapshot = true
 
 }
